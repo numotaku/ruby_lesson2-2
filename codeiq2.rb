2afcf585afa3d5a523dba1af8b6a892305435dbe
TAKU\拓也 <2841008@bemax.jp>
2016-09-12 12:53:55 +0900

    codeIQ分数計算

--- codeiq2.rb
+++ codeiq2.rb
@@ -8,5 +8,40 @@ while line = gets
 	i += 1
 	break if i==2
 end
+#最大公約数を求めるメソッド
+def gcd(a,b)
+	a,b=b,a%b while b>0
+	a
+end
+#最大公約数を求める
+a=@args[0][1]
+b=@args[1][1]
+if b>a
+	w=a
+	a=b
+	b=w
+end
+g=gcd(a,b)
+l=g*(a/g)*(b/g)
 
-p @args
+@args[0][0]=@args[0][0]*l/@args[0][1]
+@args[0][1]=l
+@args[1][0]=@args[1][0]*l/@args[1][1]
+@args[1][1]=l
+#分子同士を足す
+bunsi=@args[0][0]+@args[1][0]
+bunbo=l
+#約分する
+if bunsi>bunbo
+	g=gcd(bunsi,bunbo)
+else
+	g=gcd(bunbo,bunsi)
+end
+bunsi=bunsi/g
+bunbo=bunbo/g
+#答えを表示する
+if bunbo==1
+	puts bunsi
+else
+	puts "#{bunsi}/#{bunbo}"
+end
